ef9d7ebe9feb39352a4aeb1fc132b69f62de0f5c
stavis1 <steventavis@gmail.com>
2026-08-11 10:44:02 +0200

    added torch data wrapper for scikit-learn style inputs

---
+++ omics_PCAAE/data_processing.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Copyright (C) 2026  Steven Tavis
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+"""
+
+import torch
+
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, X):
+        self.X = torch.tensor(X, dtype = torch.bfloat16)
+    
+    def __len__(self):
+        return len(self.vectors)
+ 
+    def __getitem__(self, idx):
+        return self.X[idx, :]
+

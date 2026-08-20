5cb9c588570621d9e12b053c2c2ae89068b958c5
stavis1 <steventavis@gmail.com>
2026-08-20 10:50:45 +0200

    added parallelization of file parsing and ability to load previously generated file

--- omics_PCAAE/processing.py
+++ omics_PCAAE/processing.py
@@ -22,6 +22,7 @@ import os
 import zlib
 import tempfile
 from collections import defaultdict
+from multiprocessing import Pool
 
 import torch
 import duckdb as dd
@@ -190,9 +191,8 @@ class PartialPipeline(Pipeline):
         return self
 
 class SDDiterator:
-    def __init__(self, path, N_chunks, SDD):
-        self.path = path
-        self.chunks = list(range(N_chunks))
+    def __init__(self, SDD):
+        self.chunks = list(range(SDD.N_chunks))
         self.SDD = SDD
     
     def __iter__(self):
@@ -207,6 +207,12 @@ class SDDiterator:
         else:
             raise StopIteration
 
+def _parallel_process_chunk(*args, state):
+    obj = SparseDiskDataset.__new__(SparseDiskDataset)
+    obj.__dict__ = state
+    results = obj._process_chunk(*args)
+    return results
+
 class SparseDiskDataset:
     def __init__(self, 
                  path, 
@@ -220,7 +226,8 @@ class SparseDiskDataset:
                  mz_min = 100,
                  mz_max = 2000,
                  max_missing_frac = 0.5,
-                 min_intensity = 0):
+                 min_intensity = 0,
+                 n_jobs = 1):
         if not path.endswith('.parquet'):
             raise ValueError('path should end with .parquet')
         self.path = os.path.abspath(path)
@@ -239,14 +246,13 @@ class SparseDiskDataset:
         self.imzmls = []
         self.ibds = dict()
         self.N_chunks = 0
+        self.n_jobs = n_jobs
     
     def __len__(self):
         return self.N_chunks
     
     def __iter__(self):
-        return SDDiterator(self.path, 
-                           self.N_chunks, 
-                           self)
+        return SDDiterator(self)
     
     def __getitem__(self, idx):
         return dd.query(f'''
@@ -303,42 +309,61 @@ class SparseDiskDataset:
             del pixels['z']
         return pixels
     
+    
+    def _process_chunk(self, 
+                       tempdir, 
+                       imzml, 
+                       idx_start, 
+                       idx_stop):
+        pixels = self._read_imzml_chunk(imzml,
+                                        idx_start, 
+                                        idx_stop,
+                                        self.ibds[imzml])
+        N_pixels = pixels.shape[0]
+        quantcols = [c for c in pixels.columns if c not in self.metadata]
+        col_counts = pixels[quantcols].apply(lambda x: np.sum(x > self.min_intensity)).to_dict()
+        path = os.path.join(tempdir, str(hash((imzml, idx_start, idx_stop))) + '.parquet')
+        pixels.to_parquet(path, engine='fastparquet', index = False)
+        return (col_counts, N_pixels, path)
+    
     def read_data(self, imzml_list, ibd_list = []):
         col_counts = defaultdict(lambda:0)
         self.imzmls.extend(imzml_list)
         ibds = defaultdict(lambda:None, {mz:bd for mz,bd in zip(imzml_list, ibd_list)})
-        self.ibds.update(ibd_list)
+        self.ibds.update(ibds)
         N_pixels = 0
         parquets = []
         
         self.metadata = ['x', 'y', 'z', 'file']
         with tempfile.TemporaryDirectory() as tempdir:
+            jobs = []
             for imzml in imzml_list:
                 data = ImzMLParser(imzml)
                 n_spectra = len(data.coordinates)
                 breakpoints = list(range(0, n_spectra, self.chunksize))
                 breakpoints += [n_spectra]
-                for i in range(1, len(breakpoints)):
-                    pixels = self._read_imzml_chunk(imzml,
-                                                    breakpoints[i-1], 
-                                                    breakpoints[i],
-                                                    ibds[imzml])
-                    N_pixels += pixels.shape[0]
-                    quantcols = [c for c in pixels.columns if c not in self.metadata]
-                    counts = pixels[quantcols].apply(lambda x: np.sum(x > self.min_intensity)).to_dict()
-                    for col, count in counts.items():
-                        col_counts[col] += count
-                    path = os.path.join(tempdir, str(hash((imzml, i))) + '.parquet')
-                    pixels.to_parquet(path, engine='fastparquet', index = False)
-                    parquets.append(path)
-            if not 'z' in pixels.columns:
-                self.metadata.remove('z')
+                jobs.extend([(tempdir, imzml, breakpoints[i-1], breakpoints[i]) for i in range(1, len(breakpoints))])
+            if self.n_jobs == 1:
+                results = [self._process_chunk(*j) for j in jobs]
+            else:
+                with Pool(self.n_jobs) as p:
+                    jobs = [(j, self.__dict__) for j in jobs]
+                    results = p.starmap(_parallel_process_chunk, jobs)
+            for result in results:
+                counts, pixel_count, path = result
+                for k,v in counts:
+                    col_counts[k] += v
+                N_pixels += pixel_count
+                parquets.append(path)
             self.quantcols = [k for k,v in col_counts.items() if 1-(v/N_pixels) < self.max_missing_frac]
             self.N_chunks = int(np.ceil(N_pixels/self.chunksize))
             col_list = self.metadata + self.quantcols
 
             for parquet in parquets:
                 data = pd.read_parquet(parquet, engine='pyarrow')
+                if not 'z' in data.columns:
+                    self.metadata.remove('z')
+                    col_list.remove('z')
                 missing_cols = list(set(col_list).difference(data.columns))
                 missing = np.full((data.shape[0], len(missing_cols)), 0)
                 missing = pd.DataFrame(missing, 
@@ -360,4 +385,23 @@ class SparseDiskDataset:
                         FROM '{self.path}'
                         WHERE file = '{file}'
                         ''').df()
-
+    
+    def load_from_file(self, ibd_map = None):
+        self.ibds = defaultdict(lambda: None)
+        if ibd_map is not None:
+            self.ibds.update(ibd_map)
+        pixels = self[0]
+        self.metadata = ['x', 'y', 'z', 'file']
+        if not 'z' in pixels.columns:
+            self.metadata.remove('z')
+        self.quantcols = [c for c in pixels.columns if not c in self.metadata]
+        chunks = dd.query(f'''
+                          SELECT DISTINCT chunk
+                          FROM '{self.path}'
+                          ''').df()
+        self.N_chunks = chunks.shape[0]
+        files = dd.query(f'''
+                         SELECT DISTINCT file
+                         FROM '{self.path}'
+                         ''').df()
+        self.imzmls = list(files['file'])
